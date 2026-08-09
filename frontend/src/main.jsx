a26b84dd191bb8a65a97f933afce7de6d41b4e24
Bouchta Bilal <bilal.bouchta@student.ehb.be>
2026-08-09 15:14:39 +0200

    App inpakken in MsalProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { MsalProvider } from "@azure/msal-react";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./auth/AuthContext.jsx";
+import { msalInstance } from "./auth/msalInstance.js";
 
 // AuthProvider moet binnen BrowserRouter staan zodat login/logout kan navigeren
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <MsalProvider instance={msalInstance}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </MsalProvider>
   </StrictMode>,
 );
